1f889024a7b8d9733d0836b289a260dc768cebd0
Larry Battle <larry.battle.tech@gmail.com>
2020-10-12 21:49:00 -0500

    Create _who.cmd

---
+++ batch/raw/_who.cmd
@@ -0,0 +1,9 @@
+REM Print who you are then exit. Useful when you forgot who you're login as.  
+REM Date created: Oct 12, 2020
+@echo off
+
+echo USERDOMAIN=%USERDOMAIN%
+echo USERNAME=%USERNAME%
+echo USERPROFILE=%USERPROFILE%
+whoami
+pause
