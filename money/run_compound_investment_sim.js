74cad119c256e8d65dde81a012839b8ac60d25a4
Larry Battle <larry.battle.tech@gmail.com>
2020-12-17 22:42:40 -0600

    Update run_compound_investment_sim.js

--- money/run_compound_investment_sim.js
+++ money/run_compound_investment_sim.js
@@ -1,24 +1,110 @@
 // simulate investment year
 // date created: Dec 13, 2020
 // author: larry battle
+// TODO CLEAN_CODE: Refactor using OOP
+// TODO CLEAN_CODE: Split the reporting and simulation into separate js libraries
+// TODO NICE_TO_HAVE: Use deno 
+// TODO Look into using the Intl for currency
+
+const TAX_RATE = .3;
+
+function createFormattedObject(o){
+    const formattedObject = JSON.parse(JSON.stringify(o));
+
+    for(let [key, value] of Object.entries(formattedObject)){
+        if( (/Rate$/).test(key)){
+            formattedObject[key] = +value.toFixed(2);
+        }
+        if( (/Percentage$/).test(key)){
+            formattedObject[key] = (100 * value).toFixed(0) + '%';
+        }
+        if( (/Amount|Value$/).test(key)){
+            formattedObject[key] = formatCurrency(value);
+        }
+        if( typeof value === 'object'){
+            formattedObject[key] = createFormattedObject(value);
+        }
+    }
+
+    return formattedObject;
+}
+
+function requirePositiveNum(name, num){
+    if(!num || num < 0){
+        throw new Error(`(${name}, value => ${num}) is not positive`);
+    }
+}
+
+function computeTax(startingValue, currentValue, taxRate){
+    requirePositiveNum("startingValue", startingValue);
+    requirePositiveNum("currentValue", currentValue);
+    requirePositiveNum("taxRate", taxRate);
+
+    const diffValue = currentValue - startingValue;
+    const profitValue = (0 < diffValue) ? diffValue : 0;
+    const taxValue = profitValue * taxRate; 
+    const afterTaxCurrentValue = currentValue - taxValue;
+    const profitAfterTaxValue = profitValue * (1 - taxRate);
+    const afterTaxIncreaseRate = afterTaxCurrentValue / startingValue;
+
+    return {
+        startingValue,
+        currentValue,
+        taxRate,
+        profitValue,
+        profitAfterTaxValue,
+        afterTaxCurrentValue,
+        taxValue,
+        afterTaxIncreaseRate
+    };
+}
+
+function simulateInvestmentYear(year, startingAmount, annualPercReturns, taxRate){
+    let percIncrease = annualPercReturns[0] + (Math.random() * (annualPercReturns[1] - annualPercReturns[0]));
+    let incRate = (1+percIncrease);
+    let currentValue = startingAmount;
+    currentValue *= incRate;
+    currentValue = ~~currentValue;
+
+    if(taxRate < 0 || 1 < taxRate){
+        throw new Error(`Invalid tax rate : ${taxRate}`);
+    }
+
+    const taxInfo = computeTax(startingAmount, currentValue, taxRate);
+
+    return {
+        year,
+        incRate,
+        startingAmount,
+        beforeTax: {
+            cashAmount : currentValue
+        },
+        afterTax : taxInfo
+    };
+}
 
 function simulateInvestment(startingAmount, annualPercReturns, investingYears){
-    // console.log(`startingAmount: ${startingAmount}, annualPercReturns : ${annualPercReturns.join(',')}, investingYears: ${investingYears}`);
+    console.log(`startingAmount: ${startingAmount}, annualPercReturns : ${annualPercReturns.join(',')}, investingYears: ${investingYears}`);
     let currentValue = startingAmount;
     
     if(investingYears < 1 || 50 < investingYears){
         throw new Error(`Investing Years much be between 1 and 50 years`);
     }
 
-    // console.debug(`Year: ${0}, currentValue = $ ${currentValue.toLocaleString()} USD`);
+    console.debug(`Year: ${0}, currentValue = $ ${currentValue.toLocaleString()} USD`);
 
-    for(var i = 0; i < investingYears; i++){
-        let percIncrease = annualPercReturns[0] + (Math.random() * (annualPercReturns[1] - annualPercReturns[0]));
-        let incRate = (1+percIncrease);
-        currentValue *= incRate;
-        currentValue = ~~currentValue;
-        // console.debug(`Year: ${1 + i}, incRate = ${(percIncrease * 100).toFixed(0)}%, currentValue = $ ${currentValue.toLocaleString()} USD`);
+    const yearEvents = [];
+
+    for(var i = 0; i < investingYears; i++) {
+        const event = simulateInvestmentYear(i + 1, currentValue, annualPercReturns, TAX_RATE);
+        currentValue = event.afterTax.afterTaxCurrentValue;
+        yearEvents.push(event);
     }
+    console.debug(`currentValue: %s, metaData: %s`, formatCurrency(currentValue), JSON.stringify(yearEvents, null, 2));
+    
+    const formattedYearsEvents = yearEvents.map(y => createFormattedObject(y));
+    console.debug(`currentValue: %s, metaData: %s`, formatCurrency(currentValue), JSON.stringify(formattedYearsEvents, null, 2));
+    // TODO return yearEvents
     return currentValue;
 }
 
@@ -45,7 +131,7 @@ function reportGuess(values, startingAmount, annualPercReturns, investingYears){
 
 function runSimulationForXYears(startingAmount, annualPercReturns, investingYears){
     const guesses = [];
-    const runCount = 10000;
+    const runCount = 1;
     
     for(var i = 0; i < runCount; i++){
         guesses.push(simulateInvestment( startingAmount, annualPercReturns, investingYears));
@@ -57,18 +143,15 @@ function runSimulationForXYears(startingAmount, annualPercReturns, investingYear
 }
 
 function init(){
-    runSimulationForXYears(1250, [0.05, 0.20], 5);
-    runSimulationForXYears(1250, [0.05, 0.20], 10);
+    // runSimulationForXYears(2000, [0.05, 0.20], 5);
+
+    // runSimulationForXYears(1250, [0.05, 0.20], 5);
+    // runSimulationForXYears(1250, [0.05, 0.20], 10);
 
     runSimulationForXYears(800, [0.20, 0.50], 5);
-    runSimulationForXYears(800, [0.20, 0.50], 10);
-
-
-    runSimulationForXYears(1000, [0.20, 0.70], 5);
-    runSimulationForXYears(1000, [0.20, 0.70], 10);
-
+//     runSimulationForXYears(800, [0.20, 0.50], 10);
     
-    runSimulationForXYears(200000, [0.30, 0.60], 3);
-    runSimulationForXYears(200000, [0.30, 0.60], 5);
+    // runSimulationForXYears(200000, [0.30, 0.60], 3);
+    // runSimulationForXYears(200000, [0.30, 0.60], 5);
 }
 init();
